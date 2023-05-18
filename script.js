fd7b791f787fe7318a06608337a13ff15bdbd139
Whale Works <oliverfpower@gmail.com>
2023-05-17 19:03:14 -0700

    function works correctly

--- script.js
+++ script.js
@@ -15,20 +15,31 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  // this selects and defines the text area
   var passwordText = document.querySelector("#password");
-
+// this puts the password in the text area 
   passwordText.value = password;
-
-//questions for password parameters
+}
+// function to generate a password
 function generatePassword() {
-  //password length
-  var passLength = prompt("What password length do you want?") 
+  //password length prompts user to enter pass length
+  var passLength = prompt("What password length do you want?")
+  // changes password length to integers
+  //converting string to integer
+  //opposite .toString would convert it back to a string from an integer
   passLengthInt = parseInt(passLength)
-  //setting password length requirements
-  if (passLengthInt < 8) {
-    alert("password must be greater than 8 characters")
-    return null;
-  } 
+
+ //creates logic to keep password from being too short
+//  if (passLengthInt < 8) {
+//   alert("password must be greater than 8 characters")
+//   return null;
+//}
+//creates logic to keep password from being too long
+if (passLengthInt > 128 || passLengthInt < 8) {
+  alert("password length is either too short or too long")
+  return null;
+}
+
   //special characters y/n
   var specialConfirm = confirm("Do you want to use special characters?")
   //lowercase characters y/n
@@ -37,39 +48,50 @@ function generatePassword() {
   var upperCase = confirm("Do you want to use uppercase characters?")
   //include numbers y/n
   var numbersConfirm = confirm("Do you want to use numbers?")
-}
 
-  const lower = "abcdefghijklmnopqrstuvwxyz".split('')
-  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
-  const nums = "123456789".split('')
-  const specials = "!@#$%^&*()-_=+/?\|][}{;:'><.,`~".split('')
-  
+  if(!lowerCase && !upperCase && !numbersConfirm && !specialConfirm) {
+    alert("you silly goose there's no password here!")
+    return null;
+  }
+
+  //starting string for password characters
+  let allChar = ""
 
   if (lowerCase) {
-    lowerIndex = Math.floor(Math.random() * lower.length)
-    lowerC = lower[lowerIndex]
-  } else {
-    return !lowerCase;
+    // adds lower case letters that can be accessed for password
+    allChar += "abcdefghijklmnopqrstuvwxyz"
   }
 
   if (upperCase) {
-    upperIndex = Math.floor(Math.random() * upper.length)
-    upperC = upper[upperIndex]
-  } else {
-    upper * 0
+    // adds upper case letters that can be accessed for password
+    allChar += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
 
   if (numbersConfirm) {
-    numbersIndex = Math.floor(Math.random() * nums.length)
-    numsC = nums[numbersIndex]
-  } else {
-    nums * 0
+    //adds numbers that can be accessed for password
+    allChar += "1234567890"
+  }
+
+  if (specialConfirm) {
+    //adds special characters that can be accessed for password
+    allChar += "!@#$%^&*()-_=+/?\|][}{;:'><.,`~"
+  }
+  
+  //a password that will contain all characters grabbed for allChar 
+  //randomly grab a char and store it grab and store
+
+  let selectedChar = ""
+
+  for (let i = 0; i < passLength; i++) {
+    selectedChar += allChar[Math.floor(Math.random() * allChar.length)]
   }
 
   console.log(passLength, upperCase, lowerCase, specialConfirm, numbersConfirm)
-  return passLength;
+  return selectedChar;
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
